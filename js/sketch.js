1bfc36431d530ab63226e56be8a4bbd705f95493
yasai wang <yasaiwang@yasaideMacBook-Pro.local>
2018-08-08 07:05:02 +0800

    update

--- js/sketch.js
+++ js/sketch.js
@@ -2,7 +2,8 @@ var lengths = 1000;
 let graphics;
 var myCanvas;
 var img;
-var a1,a2;
+var a1,a2,a3;
+var rx,ry,rz;
 
 
 function setup() {    
@@ -28,25 +29,13 @@ function hexToRgb(hex) {
 
 
 function draw() {
-  rotateY(PI/10);
-
-  a1 = map(mouseX,-options.Radius,options.Radius,-TWO_PI,TWO_PI);
-  a2 = map(mouseY,-options.Radius,options.Radius,-TWO_PI,TWO_PI);
-
-  if(dist(mouseX,mouseY,width/2,height/2)<options.Radius){
-    rotateX(millis() * 0.001 * options.Speed+a2);
-    rotateY(millis() * 0.0015 * options.Speed+a1);
-  }else{
-    rotateX(millis() * 0.001 * options.Speed);
-    rotateY(millis() * 0.0015 * options.Speed);
-  }
-
-
   if(options.SavePNG == true){
     background(0,0,0,0);
   }else{
     background(options.Background);
   }
+
+  howrotate();
 
   if(options.Pattern == 'Pattern6' && type != 'image'){
     stroke(options.Color1);
@@ -56,7 +45,15 @@ function draw() {
       options.Smooth = 15;
     }
     chooseShape(options.Smooth/5);
-  }else if(options.Pattern != 'Pattern6' && type != 'image') {
+  }else if(options.Pattern == 'Pattern7' && type != 'image'){
+    stroke(options.Color1);
+    strokeWeight(options.StrokeWeight);
+    fill(options.Background);
+    if(options.Smooth < 15){
+      options.Smooth = 15;
+    }
+    chooseShape(options.Smooth/5);
+  }else if(options.Pattern != 'Pattern6' &&options.Pattern != 'Pattern7' && type != 'image') {
     fill(options.Color1);
     texture(graphics);
     pattern();
@@ -138,12 +135,92 @@ function chooseShape(detail){
 }
 
 
+function howrotate(){
+ a1 = map(mouseX,-options.Radius,options.Radius,-TWO_PI,TWO_PI);
+ a2 = map(mouseY,-options.Radius,options.Radius,-TWO_PI,TWO_PI);
+ a3 = map(mouseY,-options.Radius,options.Radius,-TWO_PI,TWO_PI);
 
+ rx = degrees(millis() * 0.00001 * options.Speed);
+ ry = degrees(millis() * 0.00001 * options.Speed);
+ rz = degrees(millis() * 0.00001 * options.Speed);
 
+ if(degrees(options.RotateX ==true && options.RotateY == true && options.RotateZ == true)){
+  rotateX(rx);
+  rotateY(ry);
+  rotateZ(rz);
 
+  if(dist(mouseX,mouseY,width/2,height/2)<options.Radius){
+    rotateX(rx+a1);
+    rotateY(ry+a2);
+    rotateZ(rz+a3);
+  }
+}
+else if(degrees(options.RotateX == false && options.RotateY == true && options.RotateZ == true)){
+  rotateX(0);
+  rotateY(ry);
+  rotateZ(rz);
 
+  if(dist(mouseX,mouseY,width/2,height/2)<options.Radius){
+        // rotateX(rx+a1);
+        rotateY(ry+a2);
+        rotateZ(rz+a3);
+      }
+    }else if(degrees(options.RotateX == true && options.RotateY == false && options.RotateZ == true)){   
+     rotateX(rx);
+     rotateY(0);
+     rotateZ(rz);
 
+     if(dist(mouseX,mouseY,width/2,height/2)<options.Radius){
+       rotateX(rx+a1);
+        // rotateY(ry+a2);
+        rotateZ(rz+a3);
+      }
+    }else if(degrees(options.RotateX == true && options.RotateY == true && options.RotateZ == false)){   
+     rotateX(rx);
+     rotateY(ry);
+     rotateZ(0);
 
-
-
-
+     if(dist(mouseX,mouseY,width/2,height/2)<options.Radius){
+       rotateX(rx+a1);
+       rotateY(ry+a2);
+        // rotateZ(rz+a3);
+      }
+    }else if(degrees(options.RotateX == false && options.RotateY == false&& options.RotateZ == true)){  
+     rotateX(0);
+     rotateY(0);
+     rotateZ(rz);
+     if(dist(mouseX,mouseY,width/2,height/2)<options.Radius){
+       rotateZ(rz+a3);
+     }
+   }else if(degrees(options.RotateX == false && options.RotateY == true&& options.RotateZ == false)){  
+    rotateX(0);
+    rotateY(ry);
+    rotateZ(0);
+    if(dist(mouseX,mouseY,width/2,height/2)<options.Radius){
+      rotateY(ry+a1);
+    }
+  }else if(degrees(options.RotateX == true  && options.RotateY == false && options.RotateZ == false)){  
+    rotateX(rx);
+    rotateY(0);
+    rotateZ(0);
+    if(dist(mouseX,mouseY,width/2,height/2)<options.Radius){
+      rotateX(rx+a1);
+    }
+  }else if(degrees(options.RotateX == true  && options.RotateY == false && options.RotateZ == false)){  
+    rotateX(rx);
+    rotateY(0);
+    rotateZ(0);
+    if(dist(mouseX,mouseY,width/2,height/2)<options.Radius){
+      rotateX(rx+a1);
+    }
+  }else if(degrees(options.RotateX == false  && options.RotateY == false && options.RotateZ == false)){  
+    rotateX(0);
+    rotateY(0);
+    rotateZ(0);
+    if(dist(mouseX,mouseY,width/2,height/2)<options.Radius){
+      rotateX(a1);
+      rotateY(a2);
+      rotateZ(a3);
+    }
+  }
+}
